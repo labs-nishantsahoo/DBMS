cbedbf6e2d7f154175edb6a0ac448d507a289396
labs-nishantsahoo <30486569+labs-nishantsahoo@users.noreply.github.com>
2017-07-27 10:37:13 +0530

    Update

--- Lab_1/ScientificCalculator/ScientificCalculator/Form1.Designer.cs
+++ Lab_1/ScientificCalculator/ScientificCalculator/Form1.Designer.cs
@@ -37,56 +37,62 @@
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
-            this.operand1 = new System.Windows.Forms.TextBox();
-            this.operand2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
             this.result = new System.Windows.Forms.Label();
+            this.displayTextBox = new System.Windows.Forms.TextBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(67, 174);
+            this.button1.Location = new System.Drawing.Point(68, 174);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "+";
+            this.button1.Text = "9";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(148, 174);
+            this.button2.Location = new System.Drawing.Point(149, 174);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "-";
+            this.button2.Text = "0";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(229, 174);
+            this.button3.Location = new System.Drawing.Point(230, 175);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
-            this.button3.Text = "*";
+            this.button3.Text = "+";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(310, 174);
+            this.button4.Location = new System.Drawing.Point(311, 174);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 3;
-            this.button4.Text = "/";
+            this.button4.Text = "-";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(310, 125);
+            this.button5.Location = new System.Drawing.Point(67, 258);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 4;
@@ -96,7 +102,7 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(67, 204);
+            this.button6.Location = new System.Drawing.Point(68, 203);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(75, 23);
             this.button6.TabIndex = 5;
@@ -106,7 +112,7 @@
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(148, 204);
+            this.button7.Location = new System.Drawing.Point(149, 203);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(75, 23);
             this.button7.TabIndex = 6;
@@ -116,12 +122,13 @@
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(229, 204);
+            this.button8.Location = new System.Drawing.Point(230, 204);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(75, 23);
             this.button8.TabIndex = 7;
-            this.button8.Text = "tan";
+            this.button8.Text = "x";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -129,64 +136,132 @@
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(75, 23);
             this.button9.TabIndex = 8;
-            this.button9.Text = "x^2";
+            this.button9.Text = "/";
             this.button9.UseVisualStyleBackColor = true;
             this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
-            // operand1
+            // resultTextBox
             // 
-            this.operand1.Location = new System.Drawing.Point(67, 65);
-            this.operand1.Name = "operand1";
-            this.operand1.Size = new System.Drawing.Size(100, 20);
-            this.operand1.TabIndex = 9;
-            this.operand1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // operand2
-            // 
-            this.operand2.Location = new System.Drawing.Point(67, 128);
-            this.operand2.Name = "operand2";
-            this.operand2.Size = new System.Drawing.Size(100, 20);
-            this.operand2.TabIndex = 10;
-            this.operand2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(64, 36);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 13);
-            this.label1.TabIndex = 11;
-            this.label1.Text = "Operand 1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(64, 100);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 13);
-            this.label2.TabIndex = 12;
-            this.label2.Text = "Operand 2";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.resultTextBox.Location = new System.Drawing.Point(286, 261);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(100, 20);
+            this.resultTextBox.TabIndex = 9;
+            this.resultTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // result
             // 
             this.result.AutoSize = true;
-            this.result.Location = new System.Drawing.Point(67, 260);
+            this.result.Location = new System.Drawing.Point(237, 268);
             this.result.Name = "result";
             this.result.Size = new System.Drawing.Size(43, 13);
             this.result.TabIndex = 14;
             this.result.Text = "Result :";
+            this.result.Click += new System.EventHandler(this.result_Click);
+            // 
+            // displayTextBox
+            // 
+            this.displayTextBox.Location = new System.Drawing.Point(67, 43);
+            this.displayTextBox.Name = "displayTextBox";
+            this.displayTextBox.Size = new System.Drawing.Size(318, 20);
+            this.displayTextBox.TabIndex = 15;
+            this.displayTextBox.TextChanged += new System.EventHandler(this.displayTextBox_TextChanged);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(68, 144);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 16;
+            this.button10.Text = "5";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(149, 145);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 17;
+            this.button11.Text = "6";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(230, 146);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 18;
+            this.button12.Text = "7";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(311, 144);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 23);
+            this.button13.TabIndex = 19;
+            this.button13.Text = "8";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(311, 115);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(75, 23);
+            this.button14.TabIndex = 20;
+            this.button14.Text = "4";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(68, 116);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(75, 23);
+            this.button15.TabIndex = 21;
+            this.button15.Text = "1";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(149, 116);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(75, 23);
+            this.button16.TabIndex = 22;
+            this.button16.Text = "2";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(230, 115);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(75, 23);
+            this.button17.TabIndex = 23;
+            this.button17.Text = "3";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
             // 
             // form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(440, 320);
+            this.ClientSize = new System.Drawing.Size(442, 320);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.displayTextBox);
             this.Controls.Add(this.result);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.operand2);
-            this.Controls.Add(this.operand1);
+            this.Controls.Add(this.resultTextBox);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
@@ -215,11 +290,17 @@
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.TextBox operand1;
-        private System.Windows.Forms.TextBox operand2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox resultTextBox;
         private System.Windows.Forms.Label result;
+        private System.Windows.Forms.TextBox displayTextBox;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
     }
 }
 
--- Lab_1/ScientificCalculator/ScientificCalculator/Form1.cs
+++ Lab_1/ScientificCalculator/ScientificCalculator/Form1.cs
@@ -12,6 +12,11 @@ namespace ScientificCalculator
 {
     public partial class form1 : Form
     {
+        string input = "";
+        string firstOperand = "";
+        string secondOperand = "";
+        char operation = ' ';
+
         public form1()
         {
             InitializeComponent();
@@ -20,6 +25,221 @@ namespace ScientificCalculator
         private void Calculator_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            displayTextBox.Text += "+";
+            operation = '+';
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            displayTextBox.Text += "-";
+            operation = '-';
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (operation == ' ')
+            {
+                firstOperand += "0";
+            }
+            else
+            {
+                secondOperand += "0";
+            }
+            displayTextBox.Text += "0";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (operation == ' ')
+            {
+                firstOperand += "9";
+            }
+            else
+            {
+                secondOperand += "9";
+            }
+            displayTextBox.Text += "9";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            displayTextBox.Text += "/";
+            operation = '/';
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double op1 = Convert.ToDouble(firstOperand);
+            double op2 = Convert.ToDouble(secondOperand);
+            firstOperand = "";
+            secondOperand = "";
+            double finalResult = 0;
+            switch(operation)
+            {
+                case '+': finalResult = op1 + op2;      
+                          break;
+
+                case '-': finalResult = op1 - op2;
+                          break;
+                
+                case '*': finalResult = op1 * op2;      
+                          break;
+
+                case '/': finalResult = op1 / op2;
+                          break;
+            }
+            displayTextBox.Text = "";
+            resultTextBox.Text = finalResult.ToString();
+            operation = ' ';
+        }
+
+        private void result_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (operation == ' ')
+            {
+                firstOperand += "1";
+            }
+            else
+            {
+                secondOperand += "1";
+            }
+            displayTextBox.Text += "1";
+        }
+
+        private void displayTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            if (operation == ' ')
+            {
+                firstOperand += "2";
+            }
+            else
+            {
+                secondOperand += "2";
+            }
+            displayTextBox.Text += "2";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            if (operation == ' ')
+            {
+                firstOperand += "3";
+            }
+            else
+            {
+                secondOperand += "3";
+            }
+            displayTextBox.Text += "3";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (operation == ' ')
+            {
+                firstOperand += "4";
+            }
+            else
+            {
+                secondOperand += "4";
+            }
+            displayTextBox.Text += "4";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (operation == ' ')
+            {
+                firstOperand += "5";
+            }
+            else
+            {
+                secondOperand += "5";
+            }
+            displayTextBox.Text += "5";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (operation == ' ')
+            {
+                firstOperand += "6";
+            }
+            else
+            {
+                secondOperand += "6";
+            }
+            displayTextBox.Text += "6";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (operation == ' ')
+            {
+                firstOperand += "7";
+            }
+            else
+            {
+                secondOperand += "7";
+            }
+            displayTextBox.Text += "7";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (operation == ' ')
+            {
+                firstOperand += "8";
+            }
+            else
+            {
+                secondOperand += "8";
+            }
+            displayTextBox.Text += "8";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            displayTextBox.Text += "*";
+            operation = '*';
         }
     }
 }
