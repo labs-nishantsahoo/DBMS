1eae23e19db6530867a428370272c67f37a6a43b
labs-nishantsahoo <30486569+labs-nishantsahoo@users.noreply.github.com>
2017-08-18 14:30:34 +0530

    LAB_3 Update

--- Lab_3/lab_3_lab_ex_1_part2.sql
+++ Lab_3/lab_3_lab_ex_1_part2.sql
@@ -3,3 +3,5 @@ INSERT INTO CAR3244 VALUES ('&regno','&model','&year');
 INSERT INTO ACCIDENT3244 VALUES ('&report_number','&accd_date','&location');
 INSERT INTO OWNS3244 VALUES ('&report_number','&regno');
 INSERT INTO PARTICIPATED3244 VALUES ('&driver_id','&regno','&report_number','&damage_amount');
+
+commit;
