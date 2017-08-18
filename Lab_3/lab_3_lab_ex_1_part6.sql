be119d4631e14f1fd289085f77d8c007d94be970
labs-nishantsahoo <30486569+labs-nishantsahoo@users.noreply.github.com>
2017-08-18 15:44:41 +0530

    LAB_4

--- Lab_3/lab_3_lab_ex_1_part6.sql
+++ Lab_3/lab_3_lab_ex_1_part6.sql
@@ -1,5 +1,3 @@
-INSERT INTO PERSON3244 VALUES ('&driver_id','&name','&address');
-INSERT INTO CAR3244 VALUES ('&regno','&model','&year');
-INSERT INTO ACCIDENT3244 VALUES ('&report_number','&accd_date','&location');
-INSERT INTO OWNS3244 VALUES ('&report_number','&regno');
-INSERT INTO PARTICIPATED3244 VALUES ('&driver_id','&regno','&report_number','&damage_amount');
+ALTER TABLE CAR3244 ADD CHECK (year>2000);
+
+commit;
