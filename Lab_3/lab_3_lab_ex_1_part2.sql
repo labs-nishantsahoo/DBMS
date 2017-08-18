af02abbd21df865aa409d24bf2dcbed3ecbe03d0
labs-nishantsahoo <30486569+labs-nishantsahoo@users.noreply.github.com>
2017-08-18 16:42:26 +0530

    Lab_4 Update

--- Lab_3/lab_3_lab_ex_1_part2.sql
+++ Lab_3/lab_3_lab_ex_1_part2.sql
@@ -1,7 +1,7 @@
 INSERT INTO PERSON3244 VALUES ('&driver_id','&name','&address');
 INSERT INTO CAR3244 VALUES ('&regno','&model','&year');
 INSERT INTO ACCIDENT3244 VALUES ('&report_number','&accd_date','&location');
-INSERT INTO OWNS3244 VALUES ('&report_number','&regno');
+INSERT INTO OWNS3244 VALUES ('&driver_id','&regno');
 INSERT INTO PARTICIPATED3244 VALUES ('&driver_id','&regno','&report_number','&damage_amount');
 
 commit;
--- Lab_4/lab_ex_5.sql
+++ Lab_4/lab_ex_5.sql
@@ -1,5 +1,5 @@
 SELECT n.name
-FROM person3244 n
+FROM PERSON3244 n
 LEFT JOIN PARTICIPATED3244 p ON p.driver_id = n.driver_id
 WHERE p.driver_id IS NULL;
 
